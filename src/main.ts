7f25bb53a879f08c756861981bbd45fb4749ae2a
kashimada <kashimada@KashimadaiMac.local>
2022-09-04 22:09:14 +0900

    wip: :construction:  draw pixels

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import p5 from "p5";
+import p5, { Color } from "p5";
 
 /** バブルの情報を定義する型 */
-type Bubble = {
+type Pixcel = {
   /** 位置（画面サイズに対する0〜1の相対位置） */
   pos: {
     x: number;
@@ -9,88 +9,40 @@ type Bubble = {
   };
   /** サイズ(px) */
   size: number;
-  /** 上昇スピード（画面高さに対する0〜1の相対値） */
-  speed: number;
-  /** 塗りつぶすか？ */
-  isFill: boolean;
+
+  number: number;
+
+  color: Color;
 };
 
 const sketch = (p: p5) => {
-  /** バブルの数 */
-  const COUNT = 40;
-  /** 最小サイズ（画面幅を1） */
-  const MINSIZE = 0.005;
-  /** 最大サイズ（画面幅を1） */
-  const MAXSIZE = 0.2;
-  /** 最小速度（画面の高さを1として、1フレームごとに動く量の最小値） */
-  const MINSPEED = 0.005;
-  /** 最大速度（画面の高さを1として、1フレームごとに動く量の最大値） */
-  const MAXSPEED = 0.02;
-  /** カーソル移動後、その位置からバブルを出し続ける時間（フレーム） */
-  const MOUSE_ACTIVE_FRAMES = 120;
-  /** 背景色 */
+  // const PI = "31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652712019091456485669234603486104543266482133936072602491412737245870066063155881748815209209628292540917153643678925903600113305305488204665213841469519415116094330572703657595919530921861173819326117931051185480744623799627495673518857527248912279381830119491298336733624".split("").map(s => parseInt(s, 10));
+  const PI = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8]
+  // const MAX_DIGITS = 512;
+  const PIXEL_SIZE = 11;
+  const x_number = ~~(p.windowWidth / PIXEL_SIZE)
   const BG_COLOR = "#171d21";
-  /** バブルの色 */
-  const BUBBLE_COLOR = "#77acb5";
-  /** 生成したバブルを格納する配列 */
-  let bubbles: Bubble[] = [];
-  /** 最後にカーソルが動いた時間（フレーム） */
-  let lastMouseMoved = -MOUSE_ACTIVE_FRAMES;
+  const DIGIT_COLORS = ["#ffffff", "#ffa500", "#ffff00", "#008000", "#006400", "#191970", "#dda0dd", "#b22222", "#f0e68c", "#000000"];
+  const pixels: Pixcel[] = [];
 
-  /**
-   * バブルを追加します。
-   * カーソルを動かしている場合にはカーソル位置に、停止している場合は画面下のランダムな位置に配置します。
-   */
-  const addBubble = () => {
-    // 仮想的な奥行きを決める（0=奥 ... 1=手前）
-    // 累乗することで奥の方が多めになるよう偏りをつける
-    const zDist = p.random() ** 3;
-    // カーソルの位置に配置するか？　カーソルを移動してから一定時間はカーソル位置を利用する
-    const isUseMousePos = p.frameCount - lastMouseMoved < MOUSE_ACTIVE_FRAMES;
-    // 初期位置のx, y座標を決定
-    const x = isUseMousePos
-      ? p.mouseX / p.width + p.random(-0.05, 0.05)
-      : p.random();
-    const y = isUseMousePos ? p.mouseY / p.height + p.random(-0.05, 0.05) : 1.2;
-    // バブルを追加
-    // 奥行きが手前にあるほど、サイズが大きくなり見かけの動きも早くなる
-    bubbles.push({
-      pos: { x, y },
-      size: p.map(zDist, 0, 1, MINSIZE, MAXSIZE),
-      speed: p.map(zDist, 0, 1, MINSPEED, MAXSPEED),
-      isFill: Math.random() > 0.5,
-    });
-  };
+  const addPixel = (number: number, x: number, y: number) => {
+    pixels.push({
+      pos: {x, y},
+      size: PIXEL_SIZE,
+      number: number,
+      color: p.color(DIGIT_COLORS[number])
 
-  /** 画面外に出たバブルを配列から除去する */
-  const removeOutBubbles = () => {
-    bubbles = bubbles.filter((b) => b.pos.y * p.height + b.size >= 0);
-  };
+    })
 
-  /** バブルの位置を更新する */
-  const updateBubbles = () => {
-    bubbles.forEach((b) => {
-      b.pos.y -= b.speed;
-    });
-  };
+  }
 
-  /** バブルを描画する */
-  const drawBubbles = () => {
-    bubbles.forEach((b) => {
-      // ノイズを使って左右の揺れの値を作る
-      const noise = p.noise(b.pos.x * 20, b.pos.y * 20);
-      const xShift = p.map(noise, 0, 1, -15, 15);
-      const color = p.color(BUBBLE_COLOR);
-      p.stroke(color);
-      b.isFill ? p.fill(color) : p.noFill();
-      // バブルの位置に計算したノイズを加えて円を描画する
-      p.circle(
-        b.pos.x * p.width + xShift,
-        b.pos.y * p.height,
-        b.size * p.width
-      );
-    });
-  };
+  const drwaPixels = () => {
+    pixels.forEach((pixel) => {
+      p.stroke(pixel.color);
+      p.fill(pixel.color);
+      p.rect(pixel.pos.x, pixel.pos.y, PIXEL_SIZE, PIXEL_SIZE);
+    })
+  }
 
   /** 初期化処理 */
   p.setup = () => {
@@ -101,18 +53,14 @@ const sketch = (p: p5) => {
   p.draw = () => {
     p.push();
     p.background(p.color(BG_COLOR));
-    p.blendMode(p.SCREEN);
-    removeOutBubbles();
-    while (bubbles.length < COUNT) {
-      addBubble();
+    // p.blendMode(p.SCREEN);
+    for (const number of PI) {
+      const x_pos = number * PIXEL_SIZE
+      const y_pos = ~~(number / x_number) * PIXEL_SIZE
+      addPixel(number, x_pos, y_pos)
     }
-    updateBubbles();
-    drawBubbles();
+    drwaPixels()
     p.pop();
-  };
-
-  p.mouseMoved = () => {
-    lastMouseMoved = p.frameCount;
   };
 
   p.windowResized = () => {
