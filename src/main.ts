1f6baee07e0c877a3c0af220af6406cac872cecb
kashimada <kashimada@KashimadaiMac.local>
2022-09-04 17:48:54 +0900

    wip: :construction:  add sample code of p5js

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,14 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import p5 from "p5";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const sketch = (p: p5) => {
+  p.setup = () => {
+    p.createCanvas(400, 400);
+  };
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  p.draw = () => {
+    p.background(220);
+    p.ellipse(50, 50, 80, 80);
+  };
+};
+
+new p5(sketch);
