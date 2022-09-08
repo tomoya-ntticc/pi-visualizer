5cc70d3bc6cd2505cd3e20618f90c418d6eb5529
kashimada <kashimada@KashimadaiMac.local>
2022-09-08 21:46:36 +0900

    wip: :construction:  read pi from text file

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './style.css'
+import pi_string from './pi_one_million.txt?raw'
 import p5, { Color } from "p5";
 
 /** バブルの情報を定義する型 */
@@ -13,10 +14,23 @@ type Pixcel = {
 };
 
 const sketch = (p: p5) => {
-  const PI = "31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652712019091456485669234603486104543266482133936072602491412737245870066063155881748815209209628292540917153643678925903600113305305488204665213841469519415116094330572703657595919530921861173819326117931051185480744623799627495673518857527248912279381830119491298336733624".split("").map(s => parseInt(s, 10));
+  const get_max_number = (PIXEL_SIZE: number, x_or_y: string) => {
+    if (x_or_y == "x") {
+      return Math.floor(p.windowWidth / PIXEL_SIZE)
+    }
+    else {
+      return Math.floor(p.windowHeight / PIXEL_SIZE)
+    }
+  }
 
-  // const MAX_DIGITS = 512;
+  const get_PI = (start: number = 0, end: number = 512) => {
+    return pi_string.split("").slice(start, end).map(s => parseInt(s, 10));
+  }
+
   const PIXEL_SIZE = 11;
+  let PI = get_PI(0, get_max_number(PIXEL_SIZE, "x") * get_max_number(PIXEL_SIZE, "y"));
+
+  const CENTER_DOT_SIZE = 1;
   const BG_COLOR = "#171d21";
   const DIGIT_COLORS = ["#ffffff", "#ffa500", "#ffff00", "#008000", "#006400", "#191970", "#dda0dd", "#b22222", "#f0e68c", "#000000"];
   const pixels: Pixcel[] = [];
@@ -33,12 +47,12 @@ const sketch = (p: p5) => {
   }
 
   const drawPixels = () => {
+    p.noStroke();
     pixels.forEach((pixel) => {
-      p.stroke(pixel.color);
       p.fill(pixel.color);
       p.rect(pixel.pos.x, pixel.pos.y, PIXEL_SIZE, PIXEL_SIZE);
-      p.stroke(BG_COLOR);
-      p.rect(pixel.pos.x + Math.floor(PIXEL_SIZE/2), pixel.pos.y + Math.floor(PIXEL_SIZE/2), 1, 1)
+      p.fill(BG_COLOR);
+      p.rect(pixel.pos.x + Math.floor((PIXEL_SIZE - CENTER_DOT_SIZE)/2), pixel.pos.y + Math.floor((PIXEL_SIZE - CENTER_DOT_SIZE)/2), CENTER_DOT_SIZE, CENTER_DOT_SIZE)
     })
   }
 
@@ -49,13 +63,12 @@ const sketch = (p: p5) => {
 
   /** フレームごとの描画処理 */
   p.draw = () => {
-    let x_number = Math.floor(p.windowWidth / PIXEL_SIZE)
     p.push();
     p.background(p.color(BG_COLOR));
     // p.blendMode(p.SCREEN);
     for (const [index, number] of PI.entries()) {
-      const x_pos = (index % x_number) * (PIXEL_SIZE + 1)
-      const y_pos = Math.floor(index / x_number) * (PIXEL_SIZE + 1)
+      const x_pos = (index % get_max_number(PIXEL_SIZE, "x")) * PIXEL_SIZE
+      const y_pos = Math.floor(index / get_max_number(PIXEL_SIZE, "x")) * PIXEL_SIZE;
       addPixel(number, x_pos, y_pos)
     }
     drawPixels()
@@ -71,6 +84,8 @@ const sketch = (p: p5) => {
 
   p.windowResized = () => {
     p.resizeCanvas(p.windowWidth, p.windowHeight);
+    PI = get_PI(0, get_max_number(PIXEL_SIZE, "x") * get_max_number(PIXEL_SIZE, "y"));
+    p.background(p.color(BG_COLOR));
   };
 };
 
