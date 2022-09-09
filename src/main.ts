c04e2609405d0f5e5e2ab81e59f22330cbbdb3ff
kashimada <kashimada@KashimadaiMac.local>
2022-09-09 18:19:40 +0900

    wip: :construction:  save images

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import pi_string from './pi_one_million.txt?raw'
 import p5, { Color } from "p5";
 
 const isDebug = true;
+let isExporting = false;
 
 type Pixel = {
   pos: {
@@ -14,13 +15,14 @@ type Pixel = {
 };
 
 const sketch = (p: p5) => {
+  let canvas_x = 510; // p.windowWidth
+  let canvas_y = 510; // p.swindowHeight
   let isFullscreen = false;
   let hiddenCenterDot = false;
 
-
-  const PIXEL_SIZES = [11, 21, 31];
-  const CENTER_DOT_SIZES = [3, 5, 7];
-  let current_size = 0;
+  const PIXEL_SIZES = [11, 21, 31, 51];
+  const CENTER_DOT_SIZES = [3, 5, 7, 11];
+  let current_size = 3;
   const BG_COLOR = "#171d21";
   const DIGIT_COLORS = [
     [255, 255, 255],
@@ -37,10 +39,10 @@ const sketch = (p: p5) => {
 
   const get_max_number = (PIXEL_SIZE: number, x_or_y: string) => {
     if (x_or_y == "x") {
-      return Math.floor(p.windowWidth / PIXEL_SIZES[current_size])
+      return Math.floor(canvas_x / PIXEL_SIZES[current_size])
     }
     else {
-      return Math.floor(p.windowHeight / PIXEL_SIZES[current_size])
+      return Math.floor(canvas_y / PIXEL_SIZES[current_size])
     }
   }
 
@@ -56,7 +58,13 @@ const sketch = (p: p5) => {
     return PI
   }
 
-  let PI = get_PI(0, get_max_number(PIXEL_SIZES[current_size], "x") * get_max_number(PIXEL_SIZES[current_size], "y"));
+  let max_x_number = get_max_number(PIXEL_SIZES[current_size], "x");
+  let max_y_number = get_max_number(PIXEL_SIZES[current_size], "y");
+  let max_digit = max_x_number * max_y_number
+  let currentDigit = 0;
+
+  let PI = get_PI(0, max_digit);
+
   let pixels: Pixel[] = [];
 
   const addPixel = (number: number, x: number, y: number) => {
@@ -81,23 +89,34 @@ const sketch = (p: p5) => {
 
   /** 初期化処理 */
   p.setup = () => {
-    p.frameRate(60);
-    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.frameRate(10);
+    p.createCanvas(canvas_x, canvas_y);
   };
 
   /** フレームごとの描画処理 */
   p.draw = () => {
-    p.push();
-    p.background(p.color(BG_COLOR));
-    rotate_PI(PI)
-    pixels = [];
-    for (const [index, number] of PI.entries()) {
-      const x_pos = (index % get_max_number(PIXEL_SIZES[current_size], "x")) * PIXEL_SIZES[current_size]
-      const y_pos = Math.floor(index / get_max_number(PIXEL_SIZES[current_size], "x")) * PIXEL_SIZES[current_size];
-      addPixel(number, x_pos, y_pos)
+    if (currentDigit < max_digit) {
+      p.push();
+      p.background(p.color(BG_COLOR));
+      rotate_PI(PI)
+      pixels = [];
+      for (const [index, number] of PI.entries()) {
+        const x_pos = (index % max_x_number) * PIXEL_SIZES[current_size]
+        const y_pos = Math.floor(index / max_x_number) * PIXEL_SIZES[current_size];
+        addPixel(number, x_pos, y_pos)
+      }
+      drawPixels()
+      p.pop();
+      if (isExporting) {
+        let filename = "pi-" + ('00000' + currentDigit).slice(-5) + ".png";
+        p.save(filename);
+        console.log("exported " + filename);
+      }
+      currentDigit++;
     }
-    drawPixels()
-    p.pop();
+    else {
+      console.log("finished rotating all digits");
+    }
     if (isDebug) {
       console.log("PI.length:" + PI.length + ", framerate: " + p.frameRate());
     }
@@ -105,16 +124,21 @@ const sketch = (p: p5) => {
 
   p.keyReleased = () => {
     if (p.key == "f") {
-      isFullscreen = !isFullscreen
-      p.fullscreen(isFullscreen)
+      isFullscreen = !isFullscreen;
+      p.fullscreen(isFullscreen);
     }
     if (p.key == "i") {
-      PI = get_PI(0, get_max_number(PIXEL_SIZES[current_size], "x") * get_max_number(PIXEL_SIZES[current_size], "y"))
+      PI = get_PI(0, max_digit);
+    }
+    if (p.key == "s") {
+      PI = get_PI(0, max_digit);
+      isExporting = true;
+      currentDigit = 0;
     }
   }
 
   p.touchEnded = () => {
-    if (current_size < PIXEL_SIZES.length) {
+    if (current_size < PIXEL_SIZES.length - 1) {
       current_size++;
     }
     else {
@@ -122,11 +146,13 @@ const sketch = (p: p5) => {
     }
   }
 
-  p.windowResized = () => {
-    p.resizeCanvas(p.windowWidth, p.windowHeight);
-    PI = get_PI(0, get_max_number(PIXEL_SIZES[current_size], "x") * get_max_number(PIXEL_SIZES[current_size], "y"));
-    p.background(p.color(BG_COLOR));
-  };
+  // p.windowResized = () => {
+  //   p.resizeCanvas(p.windowWidth, p.windowHeight);
+  //   max_x_number = get_max_number(PIXEL_SIZES[current_size], "x");
+  //   max_y_number = get_max_number(PIXEL_SIZES[current_size], "y");
+  //   PI = get_PI(0, max_digit);
+  //   p.background(p.color(BG_COLOR));
+  // };
 };
 
 new p5(sketch);
